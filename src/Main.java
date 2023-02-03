8778d34e03d9f8f8c33a2f4917b0e3bf5618b1e1
Arindam Saha <asaha15071998@gmail.com>
2023-02-03 14:33:59 +0530

    Adding Length Calculate Feature

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    static double length;
     public static void main(String[] args) {
 
         System.out.println("Welcome to Line Comparison Computation Program");
         System.out.println("..............................................");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter x1");
+        double x1 = sc.nextDouble();
+        System.out.println("Please Enter y1");
+        double y1 =sc.nextDouble();
+        System.out.println("Please Enter x2");
+        double x2 =sc.nextDouble();
+        System.out.println("Please Enter y2");
+        double y2 =sc.nextDouble();
+
+        length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println("Length="+length);
+
+
+
     }
 }
