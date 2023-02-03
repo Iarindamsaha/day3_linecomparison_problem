ebe78e14620341b195808cfd52088dcc7d6f3a53
Arindam Saha <asaha15071998@gmail.com>
2023-02-03 20:17:25 +0530

    Added Equality Check Feature

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-    static double length;
+
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Line Comparison Computation Program");
@@ -9,18 +10,40 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Please Enter x1");
-        double x1 = sc.nextDouble();
+        int x1 = sc.nextInt();
         System.out.println("Please Enter y1");
-        double y1 =sc.nextDouble();
+        int y1 =sc.nextInt();
         System.out.println("Please Enter x2");
-        double x2 =sc.nextDouble();
+        int x2 =sc.nextInt();
         System.out.println("Please Enter y2");
-        double y2 =sc.nextDouble();
+        int y2 =sc.nextInt();
 
-        length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+
+
+        Double length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
         System.out.println("Length="+length);
 
 
+        System.out.println("Please Enter x3");
+        int x3 = sc.nextInt();
+        System.out.println("Please Enter y3");
+        int y3 =sc.nextInt();
+        System.out.println("Please Enter x4");
+        int x4 =sc.nextInt();
+        System.out.println("Please Enter y4");
+        int y4 =sc.nextInt();
 
+        Double length2=Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
+        System.out.println("Length="+length);
+
+        boolean res = length2.equals(length);
+
+        if ( res == true ) {
+            System.out.println("Lines are equal");
+        }
+        else {
+            System.out.println("Lines are not equal");
+        }
+        sc.close();
     }
 }
